fee1539b6238b848a615433842238a4dd3584a25
vaibhav720 <parikhvaibhav19@gmail.com>
2022-02-12 16:21:44 +0530

    Frontend

---
+++ app.js
@@ -0,0 +1,49 @@
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+/////  global variables
+
+
+
+//////    POST METHOD
+
+
+
+
+
+
+////   GET METHODS
+
+app.get("/",function(req,res){
+    res.render("index",{});
+});
+
+
+app.get("/signup",function(req,res){
+    res.render("Sign-up",{});
+});
+
+app.get("/login",function(req,res){
+    res.render("login",{});
+});
+
+
+
+
+
+////       Port 300000
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
